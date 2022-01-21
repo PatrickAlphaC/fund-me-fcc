2cdc23883aaf7f58820cc5a9cff9d5c300a9f002
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2022-01-21 08:13:33 -0500

    updated readme

--- PriceConverter.sol
+++ PriceConverter.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.0;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
-    function getPrice() public view returns(uint256){
+    // we could maket this public, but then we'd have to deploy it
+    function getPrice() internal view returns(uint256){
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
         (,int256 answer,,,) = priceFeed.latestRoundData();
          // ETH/USD rate in 18 digit 
@@ -12,7 +13,7 @@ library PriceConverter {
     }
     
     // 1000000000
-    function getConversionRate(uint256 ethAmount) public view returns (uint256){
+    function getConversionRate(uint256 ethAmount) internal view returns (uint256){
         uint256 ethPrice = getPrice();
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
         // the actual ETH/USD conversation rate, after adjusting the extra 0s.
