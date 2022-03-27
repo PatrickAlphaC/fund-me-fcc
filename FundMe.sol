de9590a108030c1329d6fa27e4abc4c4b195e373
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2022-03-27 16:45:49 -0400

    updated

--- FundMe.sol
+++ FundMe.sol
@@ -4,12 +4,17 @@ pragma solidity ^0.8.0;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
 
+error NotOwner();
+
 contract FundMe {
     using PriceConverter for uint256;
+
+    event Funded(address indexed from, uint256 amount);
     
     mapping(address => uint256) public addressToAmountFunded;
     address[] public funders;
-    address public owner;
+    // Could we make this constant? 
+    address public /* immutable */ owner;
     
     constructor() {
         owner = msg.sender;
@@ -21,6 +26,7 @@ contract FundMe {
         // require(PriceConverter.getConversionRate(msg.value) >= minimumUSD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
+        emit Funded(msg.sender, msg.value);
     }
     
     function getVersion() public view returns (uint256){
@@ -44,19 +50,50 @@ contract FundMe {
     // }
     
     modifier onlyOwner {
-        require(msg.sender == owner);
+        // require(msg.sender == owner);
+        if (msg.sender != owner) revert NotOwner();
         _;
     }
     
     function withdraw() payable onlyOwner public {
-        payable(msg.sender).transfer(address(this).balance);
         for (uint256 funderIndex=0; funderIndex < funders.length; funderIndex++){
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
         funders = new address[](0);
+        // payable(msg.sender).transfer(address(this).balance);
+        (bool success, ) = payable(msg.sender).call{value: amount}("");
+        // call vs delegatecall
+        require(success, "Transfer failed");
+    }
+    // Explainer from: https://solidity-by-example.org/fallback/
+    // Ether is sent to contract
+    //      is msg.data empty?
+    //          /   \ 
+    //         yes  no
+    //         /     \
+    //    receive()?  fallback() 
+    //     /   \ 
+    //   yes   no
+    //  /        \
+    //receive()  fallback()
+
+    fallback() external payable {
+        fund();
+    }
+
+    recieve() external payable {
+        fund();
     }
 }
+
+// Concepts we didn't cover yet (will cover in later sections)
+// 1. Enum
+// 2. Try / Catch
+// 3. Function Selector
+// 4. abi.encode / decode
+// 5. Hash with keccak256
+// 6. Yul / Assembly
 
 
 
--- PriceConverter.sol
+++ PriceConverter.sol
@@ -3,17 +3,25 @@ pragma solidity ^0.8.0;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
+// Why is this a library and not abstract?
+// Why not an interface?
 library PriceConverter {
     // we could maket this public, but then we'd have to deploy it
-    function getPrice() internal view returns(uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-        (,int256 answer,,,) = priceFeed.latestRoundData();
-         // ETH/USD rate in 18 digit 
-         return uint256(answer * 10000000000);
+    function getPrice() internal view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        );
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        // ETH/USD rate in 18 digit
+        return uint256(answer * 10000000000);
     }
-    
+
     // 1000000000
-    function getConversionRate(uint256 ethAmount) internal view returns (uint256){
+    function getConversionRate(uint256 ethAmount)
+        internal
+        view
+        returns (uint256)
+    {
         uint256 ethPrice = getPrice();
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
         // the actual ETH/USD conversation rate, after adjusting the extra 0s.
